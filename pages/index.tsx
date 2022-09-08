057fd09a2eb4f8977f4c125dcd7f89746cc44faa
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-08 20:51:50 +0900

    デフォルトのCSSを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
