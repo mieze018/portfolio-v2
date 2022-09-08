93c7a4621fdba39df41444e963d1ec4593a9afb6
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-08 22:33:29 +0900

    データ取得を外部ファイルに移動

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 
-import useSWR from 'swr'
-
-const fetcher = (url: string): Promise<any> => fetch(url).then(res => res.json());
+import TumblrData from './api/tumblr';
 const Home: NextPage = () => {
-  const api_uri = process.env.NEXT_PUBLIC_api_URI;
-  const api_Key = process.env.NEXT_PUBLIC_api_Key;
-  const Blog_name = `${process.env.NEXT_PUBLIC_Tumblr_username}.tumblr.com`;
-  const endpoint = `${api_uri}${Blog_name}/posts?api_key=${api_Key}&limit=999`
 
-  const { data, error } = useSWR(endpoint, fetcher)
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+  if (!TumblrData()) return <div>Loading...</div>
 
-  console.log(data)
   return (
     <div>
       <Head>
