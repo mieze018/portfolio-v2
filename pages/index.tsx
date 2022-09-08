f5270fa9abe66bf117452c366be4af2c6f032b32
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-09 00:34:59 +0900

    indexではpersonal workを表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, NextPage } from 'next'
 
 import Link from 'next/link'
 import { endpoint, fetcher } from 'pages/api/tumblr';
+import PersonalWork from 'pages/personal_work';
 import React from 'react';
 import { DefaultHeader } from '../components/Header';
 import { Root } from './@type/tumblr';
@@ -11,23 +12,7 @@ const Home: NextPage<{ fallbackData: Root }> = ({ fallbackData }) => {
   // if (error) return <div>Failed to load</div>
   if (!fallbackData) return <div>Loading...</div>
   return (
-    <div>
-
-      <DefaultHeader />
-      <Link href="/personal_work">personal_work</Link>
-      <Link href="/commissioned_work">commissioned_work</Link>
-      <Link href="/info">info</Link>
-      {fallbackData?.response.posts.map((post) => {
-        return (
-          <div key={post.id}>
-            <h1>{post.id}</h1>
-            <p>{post.caption}</p>
-          </div>
-        )
-      }
-      )}
-
-    </div>
+    <PersonalWork fallbackData={fallbackData} />
   )
 }
 
