99758727377f0e04c9879d5abc7694c41c8118cf
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-09 22:15:22 +0900

    Postコンポーネントをリファクタした

--- components/molecules/Post.tsx
+++ components/molecules/Post.tsx
@@ -1,46 +1,59 @@
-import Image from 'next/image'
-import tw from 'twin.macro'
-import { Photo, Post } from 'pages/@type/tumblr'
 import { PostFooter } from 'components/molecules/PostFooter'
-const Article = tw.article`flex max-w-full mb-10`
-const PhotoWrapper = tw.div`m-auto`
+import Image from 'next/image'
+import { Photo, Post } from 'pages/@type/tumblr'
+import tw, { styled } from 'twin.macro'
+
+const Article = tw.article`flex max-w-full flex-col flex-wrap justify-center px-[2.618vw] mb-[1.618vh]`
 const Caption = tw.div`mt-0 text-sm sm:text-base`
-export const PostComponent: React.FC<{ post: Post }> = ({ post }) => (
-  <>
-    <div key={post.id_string} className={`fade-wrapper`}>
-      <Article>
-        <div
-          className={`${post.photoset_layout ? 'photoset block' : post.type}`}
-        >
-          <PhotoWrapper>
-            {post.photos.map((photo: Photo, photoK: any) => {
-              if (
-                !post.tags.find((tag: string, i: any) => tag === 's-o-l-p') ||
-                photoK === post.photos.length - 1
-              ) {
-                return (
-                  <Image
-                    className="w-full"
-                    src={photo.original_size.url}
-                    alt={photo.original_size.url}
-                    key={photo.original_size.url}
-                    height={photo.original_size.height}
-                    width={photo.original_size.width}
-                  />
-                )
-              } else {
-                return null
-              }
-            })}
-          </PhotoWrapper>
-          <Caption
-            dangerouslySetInnerHTML={{
-              __html: post['caption'],
-            }}
+const FadeWrapper = tw.div`flex flex-col items-center justify-center min-h-screen top-0 w-full transition-all`
+
+const PhotoWrapper = styled.div<{ photoset?: boolean }>`
+  ${tw`m-auto`}
+  ${({ photoset }) => photoset && tw`flex flex-wrap justify-center max-w-full`}
+`
+
+export const PostComponent = ({ post }: { post: Post }) => (
+  <FadeWrapper key={post.id}>
+    <Article>
+      <PhotoWrapper photoset={!!post.photoset_layout}>
+        {post.photos.map((photo, index) => (
+          <PhotoImage
+            key={photo.original_size.url}
+            photo={photo}
+            showOnlyLastPhoto={!!post.tags.find((tag) => tag === 's-o-l-p')}
+            lastPhoto={index === post.photos.length - 1}
           />
-          <PostFooter postDate={post.date} />
-        </div>
-      </Article>
-    </div>
-  </>
+        ))}
+      </PhotoWrapper>
+      <Caption
+        dangerouslySetInnerHTML={{
+          __html: post['caption'],
+        }}
+      />
+      <PostFooter postDate={post.date} />
+    </Article>
+  </FadeWrapper>
 )
+
+const PhotoImage = ({
+  photo,
+  showOnlyLastPhoto,
+  lastPhoto,
+}: {
+  photo: Photo
+  showOnlyLastPhoto: boolean
+  lastPhoto: boolean
+}) => {
+  //最後の画像だけ表示するタグがついている場合、最後の画像でなければスキップ
+  if (showOnlyLastPhoto && lastPhoto) return <></>
+  return (
+    <Image
+      className="w-full"
+      src={photo.original_size.url}
+      alt={photo.original_size.url}
+      key={photo.original_size.url}
+      height={photo.original_size.height}
+      width={photo.original_size.width}
+    />
+  )
+}
