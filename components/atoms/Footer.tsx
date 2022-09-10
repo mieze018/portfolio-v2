8d960ac7787c41dcd61341d7d1f1cd520241ad5a
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-10 12:16:46 +0900

    Footerをリファクタ

--- components/atoms/Footer.tsx
+++ components/atoms/Footer.tsx
@@ -1,13 +1,6 @@
+import { copyright } from 'libs/copyright'
 import tw from 'twin.macro'
 
-export const Footer = () => {
-  //現在の年を取得
-  const now = new Date()
-  const this_year = now.getFullYear()
-  const Wrapper = tw.footer`bottom-0 py-0 text-xs text-center`
-  return (
-    <Wrapper>
-      © 2009-{this_year} {process.env.NEXT_PUBLIC_author}
-    </Wrapper>
-  )
-}
+const Wrapper = tw.footer`bottom-0 py-0 text-xs text-center`
+
+export const Footer = () => <Wrapper>{copyright()}</Wrapper>
