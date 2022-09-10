1834f1ab87ad152f8267597b3ab432ffdf4df461
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-11 02:21:38 +0900

    フォントを読み込んだ

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,6 +22,12 @@ export default class MyDocument extends Document {
           <React.Fragment key="styles">
             {initialProps.styles}
             {sheet.getStyleElement()}
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&display=swap"
+              rel="stylesheet"
+            />
           </React.Fragment>,
         ],
       }
