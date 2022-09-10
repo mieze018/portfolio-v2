c4c10691bd672bb95bd149b373264ed00b033bad
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-10 22:36:07 +0900

    Styled-componentにした

--- components/Molecules/TopBar.tsx
+++ components/Molecules/TopBar.tsx
@@ -2,6 +2,7 @@ import './TopBar.css'
 
 import Link from 'next/link'
 import { useCallback, useEffect, useRef } from 'react'
+import tw from 'twin.macro'
 
 import type { FC } from 'react'
 
@@ -93,31 +94,30 @@ export const TopBar: FC = () => {
     />
   )
 }
-
+const Floater = tw.div`fixed top-0 z-10 w-full bg-surface`
+const Wrapper = tw.header`fixed top-0 z-10 w-full mb-0 text-sm text-center`
+const Title = tw.h1`mb-1 text-2xl header-title text-primary xs:text-3xl tracking-title`
+const Description = tw.p`text-xs header-desc sm:text-base`
+const Nav = tw.nav`z-10 text-center`
 export const TopBarComponent: FC<{
   TitleLink: FC
   description?: string
   navLinks: FC<{ className: string }>
-}> = (props) => {
-  return (
-    <>
-      <div id="floater" className="fixed top-0 z-10 w-full bg-surface"></div>
-      <header className="fixed top-0 z-10 w-full mb-0 text-sm text-center">
-        <div id="sinker">
-          <div id="fade-outer">
-            <h1 className="mb-1 text-2xl header-title text-primary xs:text-3xl tracking-title">
-              <props.TitleLink />
-            </h1>
-
-            <p className="text-xs header-desc sm:text-base">
-              {props.description ?? process.env.NEXT_PUBLIC_description}
-            </p>
-          </div>
-          <nav className="z-10 text-center">
-            <props.navLinks className="inline-block m-2 xs:m-3 mix-blend-multiply xs:tracking-widest" />
-          </nav>
+}> = (props) => (
+  <>
+    <Floater id="floater" className="fixed top-0 z-10 w-full bg-surface" />
+    <Wrapper>
+      <div id="sinker">
+        <div id="fade-outer">
+          <Title>
+            <props.TitleLink />
+          </Title>
+          <Description>{props.description ?? process.env.NEXT_PUBLIC_description}</Description>
         </div>
-      </header>
-    </>
-  )
-}
+        <Nav>
+          <props.navLinks className="inline-block m-2 xs:m-3 mix-blend-multiply xs:tracking-widest" />
+        </Nav>
+      </div>
+    </Wrapper>
+  </>
+)
