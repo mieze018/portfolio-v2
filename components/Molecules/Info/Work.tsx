e40fb424927326e3d8f59b6f6181d97585d91bc0
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-11 02:09:17 +0900

    晴れて使えるようになったcss属性を使った

--- components/Molecules/Info/Work.tsx
+++ components/Molecules/Info/Work.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import tw from 'twin.macro'
 
 import type { work } from 'libs/@type/work'
 
 export const Work = ({ work }: { work: work }) => {
   return (
-    <li>
-      {work.author && <i className="ml-3">{work.author}</i>}
-      <span className="ml-3">『{work.title}』</span>
-      {work.publisher && <span className="ml-3">({work.publisher})</span>}
-      {work.format && <small className="ml-3">{work.format}</small>}
-      {work.designer && <small className="ml-3">{work.designer}</small>}
-      {work.releaseMonth && <small className="ml-3">- {work.releaseMonth?.split('-', 1)}</small>}
+    <li css={tw`flex gap-3`}>
+      {work.author && <i>{work.author}</i>}
+      <span>『{work.title}』</span>
+      {work.publisher && <span>({work.publisher})</span>}
+      {work.format && <small>{work.format}</small>}
+      {work.designer && <small>{work.designer}</small>}
+      {work.releaseMonth && <small>- {work.releaseMonth?.split('-', 1)}</small>}
     </li>
   )
 }
