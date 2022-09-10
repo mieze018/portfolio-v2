cee3ec7d4acd422e5d02907c02e6f902f79e744e
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-10 22:56:49 +0900

    windowがundefinedでエラーになるのでいったんコメントアウト

--- libs/tumblrLink.ts
+++ libs/tumblrLink.ts
@@ -1,26 +1,28 @@
 // TODO:絶対もっといいやり方ある
+
 export const addAgentToHtml = () => {
-  {
-    const userAgent = window.navigator.userAgent.toLowerCase()
-    //モバイルの時
-    if (
-      userAgent.includes('iphone') ||
-      userAgent.includes('ipad') ||
-      userAgent.includes('android')
-    ) {
-      document.querySelector('html')?.classList.add('mobile')
-    } else {
-      document.querySelector('html')?.classList.add('desktop')
-    }
-    userAgent.includes('android') && document.querySelector('html')?.classList.add('android')
-    // userAgent.indexOf('gecko') !== -1 &&
-    //   document.querySelector('html')?.classList.add('gecko');
-    /**スマホでアクセスした時tumblrへのリンクをアプリから開くリンクに書き換え*/
-    document
-      .querySelector('.mobile .tumblr')
-      ?.setAttribute(
-        'href',
-        `http://www.tumblr.com/open/app?app_args=blog&blogName=${process.env.NEXT_PUBLIC_Tumblr_username}&page=blog`
-      )
-  }
+  console.log()
+  // {
+  //   const userAgent = window.navigator.userAgent.toLowerCase()
+  //   //モバイルの時
+  //   if (
+  //     userAgent.includes('iphone') ||
+  //     userAgent.includes('ipad') ||
+  //     userAgent.includes('android')
+  //   ) {
+  //     document.querySelector('html')?.classList.add('mobile')
+  //   } else {
+  //     document.querySelector('html')?.classList.add('desktop')
+  //   }
+  //   userAgent.includes('android') && document.querySelector('html')?.classList.add('android')
+  //   // userAgent.indexOf('gecko') !== -1 &&
+  //   //   document.querySelector('html')?.classList.add('gecko');
+  //   /**スマホでアクセスした時tumblrへのリンクをアプリから開くリンクに書き換え*/
+  //   document
+  //     .querySelector('.mobile .tumblr')
+  //     ?.setAttribute(
+  //       'href',
+  //       `http://www.tumblr.com/open/app?app_args=blog&blogName=${process.env.NEXT_PUBLIC_Tumblr_username}&page=blog`
+  //     )
+  // }
 }
