152f8b387494f248958a2c326d0bbe9b60cbed2b
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-12 00:52:33 +0900

    return <></>にキーがなくて怒られていた

--- components/Molecules/Post/Photos.tsx
+++ components/Molecules/Post/Photos.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import type { Tumblr } from 'libs/@type/api/tumblr'
 
 import { Photo } from 'components/Molecules/Post/Photo'
@@ -16,7 +18,8 @@ export const Photos = ({
       {photos.map((photo, i) => {
         const isLastPhoto = i === photos.length - 1
         //最後の画像だけ表示するタグがついている場合、最後の画像でなければスキップ
-        if (isShowOnlyLastPhoto && !isLastPhoto) return <></>
+        if (isShowOnlyLastPhoto && !isLastPhoto)
+          return <React.Fragment key={photo.original_size.url}></React.Fragment>
         return <Photo key={photo.original_size.url} photo={photo} isColumn={isColumn} />
       })}
     </>
