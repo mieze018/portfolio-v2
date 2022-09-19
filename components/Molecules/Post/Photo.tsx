7244406a77a350f1fce38e70caf419392856200e
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-19 18:13:35 +0900

    Revert "画像をクリックしたらスクロールして拡大、ハッシュリンク"
    This reverts commit 2728013a3fa3c8a973ad5ed3822b3b04c884803a.

--- components/Molecules/Post/Photo.tsx
+++ components/Molecules/Post/Photo.tsx
@@ -1,59 +1,18 @@
 import Image from 'next/future/image'
-import Link from 'next/link'
-import { useEffect, useRef, useState } from 'react'
 import tw, { styled } from 'twin.macro'
 
 import type { Tumblr } from 'libs/@type/api/tumblr'
-import type { Dispatch, SetStateAction } from 'react'
 /** 画像に直接スタイル指定せずラッパーにflex-itemのCSSをかける */
 const FlexItem = styled.div<{ $isColumn: boolean }>`
   ${tw`w-full`}
   ${({ $isColumn }) => $isColumn && tw`flex-grow w-1/4 mx-0 my-4 basis-1/4 shrink`}
 `
-export const Photo = ({
-  photo,
-  isColumn,
-  isFocusing,
-  setIsFocusing,
-}: {
-  photo: Tumblr.Photo
-  isColumn: boolean
-  isFocusing: boolean
-  setIsFocusing: Dispatch<SetStateAction<boolean>>
-}) => {
-  const [clickedImage, setClickedImage] = useState<HTMLImageElement | null>(null)
-  const ref = useRef<HTMLDivElement>(null)
-
-  const anchorId = `photo-${photo.original_size.url.slice(-15)}`
-  const handleFocus = (e) => {
-    setIsFocusing(!isFocusing)
-    // setClickedImage(e.target)
-  }
-  useEffect(() => {
-    //パスに#が含まれていたらフォーカスしてスクロール
-    if (window.location.hash === `#${anchorId}`) {
-      setIsFocusing(true)
-      // ref.current?.scrollIntoView(true)
-
-      clickedImage?.scrollIntoView({ block: 'center', inline: 'center' })
-    } else {
-      setIsFocusing(false)
-    }
-  }, [clickedImage, isFocusing])
+export const Photo = ({ photo, isColumn }: { photo: Tumblr.Photo; isColumn: boolean }) => {
   return (
-    <Link href={`#${anchorId}`} scroll={false} shallow={true} onClick={(e) => handleFocus(e)}>
-      <FlexItem
-        id={anchorId}
-        ref={ref}
-        $isColumn={isColumn}
-        css={[
-          tw`m-auto`,
-          isFocusing && ref.current === clickedImage
-            ? tw`max-w-none cursor-zoom-out`
-            : tw`cursor-zoom-in`,
-        ]}
-      >
+    <>
+      <FlexItem $isColumn={isColumn}>
         <Image
+          className="m-auto"
           src={photo.original_size.url}
           alt={photo.original_size.url}
           key={photo.original_size.url}
@@ -63,9 +22,8 @@ export const Photo = ({
           quality={85}
           // placeholder="blur"
           // blurDataURL={photo.alt_sizes.find((size) => size.width === 100)?.url}
-          css={tw`mx-auto`}
         />
       </FlexItem>
-    </Link>
+    </>
   )
 }
--- components/Molecules/Post/Photos.tsx
+++ components/Molecules/Post/Photos.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import type { Tumblr } from 'libs/@type/api/tumblr'
-import type { Dispatch, SetStateAction } from 'react'
 
 import { Photo } from 'components/Molecules/Post/Photo'
 
@@ -9,14 +8,10 @@ export const Photos = ({
   photos,
   isShowOnlyLastPhoto,
   isColumn,
-  isFocusing,
-  setIsFocusing,
 }: {
   photos: Tumblr.Photo[]
   isShowOnlyLastPhoto: boolean
   isColumn: boolean
-  isFocusing: boolean
-  setIsFocusing: Dispatch<SetStateAction<boolean>>
 }) => {
   return (
     <>
@@ -25,15 +20,7 @@ export const Photos = ({
         //最後の画像だけ表示するタグがついている場合、最後の画像でなければスキップ
         if (isShowOnlyLastPhoto && !isLastPhoto)
           return <React.Fragment key={photo.original_size.url}></React.Fragment>
-        return (
-          <Photo
-            key={photo.original_size.url}
-            photo={photo}
-            isColumn={isColumn}
-            isFocusing={isFocusing}
-            setIsFocusing={setIsFocusing}
-          />
-        )
+        return <Photo key={photo.original_size.url} photo={photo} isColumn={isColumn} />
       })}
     </>
   )
