42c3a5db2ca71c0e69591049d7fe9b2dfc68bb10
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-20 15:43:40 +0900

    Revert "Framer motionを使った形にしてみたがDialogの本来の機能が失われるのでやめた"
    This reverts commit 9a0f9e2d15b04162612ac20be358185812d5dcf0.

--- components/Organisms/Modal.tsx
+++ components/Organisms/Modal.tsx
@@ -1,5 +1,4 @@
 import { Dialog, Transition } from '@headlessui/react'
-import { AnimatePresence, motion } from 'framer-motion'
 import { useRecoilState } from 'recoil'
 import tw from 'twin.macro'
 
@@ -11,36 +10,39 @@ export const Modal = () => {
   const ContentWrapper = tw.div`m-auto flex items-center h-screen w-screen overflow-auto`
 
   return (
-    <AnimatePresence
-      mode="wait"
-      onExitComplete={() => {
-        // ref.current?.scrollIntoView(true)
-      }}
+    <Transition
+      show={isModalOpen}
+      enter="transition duration-100 ease-out"
+      enterFrom="transform scale-95 opacity-0"
+      enterTo="transform scale-100 opacity-100"
+      leave="transition duration-75 ease-out"
+      leaveFrom="transform scale-100 opacity-100"
+      leaveTo="transform scale-95 opacity-0"
     >
-      {isModalOpen && (
-        <Dialog
-          static
-          as={motion.div}
-          open={isModalOpen}
-          onClose={() => {
-            setIsModalOpen(!isModalOpen)
-          }}
-          tw="fixed inset-0 z-50 w-full h-full overflow-auto"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.1 }}
+      <Dialog
+        tw="fixed inset-0 z-50 w-full h-full overflow-auto"
+        open={isModalOpen}
+        onClose={() => {
+          setIsModalOpen(!isModalOpen)
+        }}
+      >
+        <Transition.Child
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <Dialog.Overlay as={motion.div} tw="bg-white fixed h-full w-full opacity-95 inset-0" />
+          <Dialog.Overlay tw="bg-white fixed h-full w-full opacity-95 inset-0" />
           <Dialog.Panel
-            as={motion.div}
             tw="fixed m-auto overflow-auto cursor-pointer min-w-full min-h-full"
             onClick={() => setIsModalOpen(!isModalOpen)}
           >
             <ContentWrapper>{modalContent}</ContentWrapper>
           </Dialog.Panel>
-        </Dialog>
-      )}
-    </AnimatePresence>
+        </Transition.Child>
+      </Dialog>
+    </Transition>
   )
 }
