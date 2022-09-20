d81c96523b4118195e34fe74a8a4969bf009ced8
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-20 23:42:13 +0900

    ページディレクトリ

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,12 +3,12 @@ import type { GetStaticProps, NextPage } from 'next'
 import { InfoContent } from 'components/Organisms/InfoContent'
 import { infoData } from 'pages/api/info'
 
-const Info: NextPage<{ fallbackData: typeof infoData }> = ({ fallbackData }) => {
+const About: NextPage<{ fallbackData: typeof infoData }> = ({ fallbackData }) => {
   if (!fallbackData) return <div>Loading...</div>
   return <InfoContent data={fallbackData} />
 }
 
-export default Info
+export default About
 
 export const getStaticProps: GetStaticProps = async () => {
   const Data = infoData
