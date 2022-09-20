3d4f8a1743165a458f3ca4e0d211c40d7521624d
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-21 05:30:05 +0900

    イベントの表示がおかしかったので修正

--- pages/api/about/events.ts
+++ pages/api/about/events.ts
@@ -1,11 +1,11 @@
 import type { LocalApi } from 'libs/@type/api/local'
 
 export const events: LocalApi.Event[] = [
-  {
-    title: 'CANCAN exhibition',
-    date: '2016',
-    place: 'LemoArt Gallery (Berlin, Germany)',
-  },
+  // {
+  //   title: 'CANCAN exhibition',
+  //   date: '2016',
+  //   place: 'LemoArt Gallery (Berlin, Germany)',
+  // },
   {
     title: 'Digital Creator 23人展「恋」',
     date: '2016',
