e26260f9a3e410a0f596895c3677702167351fb8
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-21 15:11:11 +0900

    touch-action

--- styles/global.ts
+++ styles/global.ts
@@ -11,11 +11,14 @@ export const GlobalStyle = createGlobalStyle<{ userAgent: string | null }>`
     /* @apply scroll-smooth; //ページ移管時にスムーズにスクロールして欲しくない */
     ${tw`font-serif leading-normal tracking-wider text-primary accent-main`}//テキスト
     ${tw`scrollbar-thin scrollbar-thumb-main/20`}//スクロールバー
+    ${tw`touch-pan-y`}//スクロールバー
     text-size-adjust: 100%;
     text-shadow: 0 1px 0 rgb(255 255 255 / 38%), 0 2px 0 rgb(var(--color-main) / 16%);
+    touch-action: pan-y; 
     &::before{
       content: "";
     ${tw`fixed inset-0 w-screen h-screen bg-fixed bg-body -z-1`}
+    touch-action: none; 
     }
   }
   body {
