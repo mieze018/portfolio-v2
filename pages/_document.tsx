19013aee7608e914ebbc9a689dec99a6d35efb40
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-21 14:37:15 +0900

    拡大はできるけど縮小は不可にしたい

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -44,6 +44,10 @@ export default class MyDocument extends Document {
               href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&display=swap"
               rel="stylesheet"
             />
+            <meta
+              name="viewport"
+              content="width=device-width,initial-scale=1.0,minimum-scale=1.0"
+            ></meta>
           </React.Fragment>,
         ],
       }
