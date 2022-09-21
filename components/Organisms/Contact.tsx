aa9a155bb5db90a821c6b311cae25d1253edb4f7
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-21 22:24:06 +0900

    Update Contact.tsx

--- components/Organisms/Contact.tsx
+++ components/Organisms/Contact.tsx
@@ -4,7 +4,18 @@ import type { aboutData } from 'pages/api/about'
 
 const Wrapper = tw.div`px-5 text-xs leading-7 md:text-sm mb-20 grid gap-12 text-left w-fit mt-12 mx-auto gap-y-16`
 
-export const Contact = ({ data }: { data: typeof aboutData }) => {
+export const ContactContent = ({ data }: { data: typeof aboutData }) => {
   const { links, events, workExperience, genres } = data
-  return <Wrapper>contact</Wrapper>
+
+  return (
+    <Wrapper>
+      <form action="/send-data-here" method="post">
+        <label htmlFor="roll">Roll Number</label>
+        <input type="text" id="roll" name="roll" required minLength="10" maxLength="20" />
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+        <button type="submit">Submit</button>
+      </form>
+    </Wrapper>
+  )
 }
