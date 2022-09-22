62493514fa802765de119f5e124af053cfa950be
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-22 17:22:52 +0900

    フッターに追加

--- components/Molecules/Footer.tsx
+++ components/Molecules/Footer.tsx
@@ -1,7 +1,13 @@
 import tw from 'twin.macro'
 
+import { SocialLinks } from 'components/Molecules/SocialLinks'
 import { copyright } from 'libs/copyright'
 
-const Wrapper = tw.footer`bottom-0 py-0 text-xs text-center pb-4 relative top-g-38vh`
+const Wrapper = tw.footer`bottom-0 py-0 text-xs text-center pb-4 relative top-g-38vh grid gap-4`
 
-export const Footer = () => <Wrapper>{copyright()}</Wrapper>
+export const Footer = () => (
+  <Wrapper>
+    <SocialLinks />
+    <div>{copyright()}</div>
+  </Wrapper>
+)
