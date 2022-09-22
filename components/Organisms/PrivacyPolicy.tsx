885e6f922eb922227df21da536cf8531a6f61eac
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-22 22:30:12 +0900

    国際化

--- components/Organisms/PrivacyPolicy.tsx
+++ components/Organisms/PrivacyPolicy.tsx
@@ -1,33 +1,29 @@
+import { useTranslation } from 'next-i18next'
 import tw from 'twin.macro'
 
 const Wrapper = tw.div`px-5 max-w-screen-md m-auto grid gap-y-4 text-xs mb-20`
 const P = tw.p``
 
 export const PrivacyPolicy = () => {
+  const { t } = useTranslation('common')
   return (
     <Wrapper>
       <div>
-        <h2>著作権・肖像権</h2>
-        <P>
-          当サイトで掲載しているすべてのコンテンツの著作権・肖像権等は当サイト所有者または各権利所有者が保有し、許可なく無断利用することを禁止します。
-        </P>
+        <h2>{t('Copyrights and Portrait Rights')}</h2>
+        <P>{t('Copyrights and Portrait Rights P1')}</P>
       </div>
       <div>
-        <h2>個人情報の利用目的</h2>
-        <P>
-          取得した個人情報は、必要な連絡のみに利用させていただくもので、これらの目的以外では利用いたしません。
-        </P>
+        <h2>{t('Personal Information')}</h2>
+        <P>{t('Personal Information P1')}</P>
       </div>
       <div>
-        <h2>個人情報の第三者開示</h2>
-        <P>
-          取得した個人情報は適切に管理し、正当な理由がある場合を除き第三者に提供することはありません。
-        </P>
+        <h2>{t('Other Disclosures')}</h2>
+        <P>{t('Other Disclosures P1')}</P>
       </div>
       <div>
-        <h2>お問合せフォーム</h2>
+        <h2>{t('Contact Form')}</h2>
         <P>
-          当サイトでは、お問い合わせフォームにFormspreeを利用しています。
+          {t('Contact Form P1')}
           <P>
             <a
               target="_blank"
@@ -40,26 +36,23 @@ export const PrivacyPolicy = () => {
         </P>
       </div>
       <div>
-        <h2>アクセス解析</h2>
-        <P>
-          当サイトでは、サイトの分析と改善のためにGoogleが提供している「Google
-          アナリティクス」を利用しています。
-        </P>
+        <h2>{t('Access Analysis')}</h2>
+        <P>{t('Access Analysis P1')}</P>
         <P>
           <a
-            href="https://support.google.com/analytics/answer/7318509?hl=ja"
+            href="https://support.google.com/analytics/answer/7318509?hl"
             target="_blank"
             rel="noreferrer"
           >
-            プライバシーの開示に関するポリシー（アナリティクス ヘルプ）
+            {t('Privacy Disclosures Policy')}
           </a>
         </P>
       </div>
       <div>
-        <h2>プライバシーポリシーの変更</h2>
-        <P>当サイトは、本プライバシーポリシーの内容を適宜見直し、その改善に努めます。</P>
-        <P>本プライバシーポリシーは、事前の予告なく変更することがあります。</P>
-        <P>本プライバシーポリシーの変更は、当サイトに掲載された時点で有効になるものとします。</P>
+        <h2>{t('Changes To This Privacy Policy')}</h2>
+        <P>{t('Changes To This Privacy Policy P1')}</P>
+        <P>{t('Changes To This Privacy Policy P2')}</P>
+        <P>{t('Changes To This Privacy Policy P3')}</P>
       </div>
     </Wrapper>
   )
