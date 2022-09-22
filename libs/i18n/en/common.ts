bf8e386ef748a25de82badd2871fe49476c9b1e3
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-22 15:30:54 +0900

    i18nを更新、enを作成

---
+++ libs/i18n/en/common.ts
@@ -0,0 +1,15 @@
+const common = {
+  siteName: 'mieze illustration',
+  author: 'mieze',
+  author_pronunciation: 'ˈmiːt͡sə',
+  description: 'Osaka, Japan based artist / illustrator.',
+  linktree: 'Linktree',
+  acceptingWork: 'I am currently accepting work requests.',
+  eventIncoming: 'Event Schedule',
+  workExperience: 'Work Experience',
+  awards: 'Awards',
+  eventHistory: 'Past Events',
+  emailLabel: 'Email',
+}
+
+export default common
--- libs/i18n/ja/common.ts
+++ libs/i18n/ja/common.ts
@@ -4,11 +4,12 @@ const common = {
   author_pronunciation: '【ミーツェ】',
   description: '大阪在住のイラストレーター・アーティスト。',
   linktree: 'Linktree',
-  toMail: 'お仕事のご依頼は以下のメールアドレスにお願いいたします。',
+  acceptingWork: '現在お仕事のご依頼を受け付けています。',
   eventIncoming: 'イベントの参加予定',
   workExperience: '仕事の経験(敬称略)',
   awards: '受賞',
   eventHistory: '過去のイベント',
+  emailLabel: 'Email'
 }
 
 export default common
