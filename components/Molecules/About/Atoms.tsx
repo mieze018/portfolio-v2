85af77a8766828dfdf98ddb8c7f3311eafd58ea5
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-22 15:30:19 +0900

    Aboutのセクションの高さの最低幅を指定

--- components/Molecules/About/Atoms.tsx
+++ components/Molecules/About/Atoms.tsx
@@ -4,6 +4,6 @@ export const P = tw.p``
 export const SnsLink = tw.a`ml-3 tracking-wider`
 export const H2 = tw.h2`my-2 leading-loose tracking-widest`
 
-export const SectionWrapper = tw.section``
+export const SectionWrapper = tw.section`md:min-h-g-23vh`
 export const Hr = tw.hr``
 export const UlNest1 = tw.ul`grid gap-4 leading-normal tracking-wide`
