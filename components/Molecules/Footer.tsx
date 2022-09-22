d28e54d0c8d1582429217773e2d64307960df7c8
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-22 23:40:43 +0900

    下部ナビをフッターの中に入れた

--- components/Molecules/Footer.tsx
+++ components/Molecules/Footer.tsx
@@ -2,8 +2,11 @@ import { useTranslation } from 'next-i18next'
 import Link from 'next/link'
 import tw from 'twin.macro'
 
+import { Nav } from 'components/Molecules/Header/Nav'
+import { NavLinks } from 'components/Molecules/Header/NavLink'
 import { SocialLinks } from 'components/Molecules/SocialLinks'
 import { copyright } from 'libs/copyright'
+import { routes } from 'libs/routes'
 
 const Wrapper = tw.footer`bottom-0 py-0 text-xs text-center pb-4 relative top-g-38vh grid gap-4`
 
@@ -11,6 +14,9 @@ export const Footer = () => {
   const { t } = useTranslation('common')
   return (
     <Wrapper>
+      <Nav css={tw`blur-0`}>
+        <NavLinks routes={routes} />
+      </Nav>
       <SocialLinks />
       <div>{copyright()}</div>
       <div>
