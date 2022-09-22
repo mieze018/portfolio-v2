74ea2b3fd7680fc9ba3c53360146f4b36d276b20
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-22 14:02:34 +0900

    文字選択、キャレットの色を指定

--- styles/global.ts
+++ styles/global.ts
@@ -13,7 +13,7 @@ export const GlobalStyle = createGlobalStyle<{ ua?: UserAgent | null }>`
   }
   body {
     /* @apply scroll-smooth; //ページ移管時にスムーズにスクロールして欲しくない */
-    ${tw`font-serif leading-normal tracking-wider text-primary accent-main`}//テキスト
+    ${tw`font-serif leading-normal tracking-wider text-primary accent-main/60 caret-main/60`}//テキスト
     text-size-adjust: 100%;
     text-shadow: 0 1px 0 rgb(255 255 255 / 38%), 0 2px 0 rgb(var(--color-main) / 16%);
     ${(props) => (props.ua?.isDesktop ? tw`text-base` : tw`text-sm`)}
@@ -22,4 +22,7 @@ export const GlobalStyle = createGlobalStyle<{ ua?: UserAgent | null }>`
     ${tw`fixed inset-0 block w-full h-full min-h-screen bg-body -z-1`}//背景
     }
   }
+    ::selection {
+      background: rgb(var(--color-main) / 60%);
+}
 `
