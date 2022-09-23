367c90766abcdfe71184da94e5eb08ae79b1aeab
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-23 17:49:48 +0900

    不要になったファイルを削除

--- libs/notion.ts
+++ libs/notion.ts
@@ -35,22 +35,22 @@ export const getPage = async (pageId: string) => {
   return response;
 };
 
-export const getBlocks = async (blockId: string) => {
-  const blocks = [];
-  let cursor;
-  while (true) {
-    const { results, next_cursor } = await notion.blocks.children.list({
-      start_cursor: cursor,
-      block_id: blockId,
-    });
-    blocks.push(...results);
-    if (!next_cursor) {
-      break;
-    }
-    cursor = next_cursor;
-  }
-  return blocks;
-};
+// export const getBlocks = async (blockId: string) => {
+//   const blocks = [];
+//   let cursor;
+//   while (true) {
+//     const { results, next_cursor } = await notion.blocks.children.list({
+//       start_cursor: cursor,
+//       block_id: blockId,
+//     });
+//     blocks.push(...results);
+//     if (!next_cursor) {
+//       break;
+//     }
+//     cursor = next_cursor;
+//   }
+//   return blocks;
+// };
 
 export const getProperties = (object: PageObject, { name, type }: { name: string, type: propertiesTypes }) => {
   if (!object?.properties[name]) return null
--- pages/api/about/workExperiences/workExperiences.ts
+++
@@ -1,5 +0,0 @@
-import json from './workExperiences.json'
-
-import type { LocalApi } from 'libs/@type/api/local'
-
-export const workExperience: LocalApi.WorkExperience.Work[] = json
