17c05d7e2b6b83d898a558792f9d8328dbb494b3
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-23 15:13:16 +0900

    データ取得
    DBIDをファイルにまとめた

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,19 +1,43 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
+import type { PageObject } from 'libs/@type/api/notion'
 import type { GetStaticProps, NextPage } from 'next'
 
 import { AboutContent } from 'components/Organisms/AboutContent'
-import { aboutData } from 'pages/api/about'
+import { getDatabase } from 'libs/notion'
+import { prizesDBId, workExperienceDBId, workExperienceGenreDBId, eventDBId } from 'libs/notionDB'
 
-const About: NextPage<{ fallbackData: typeof aboutData }> = ({ fallbackData }) => {
+export type aboutDataType = {
+  fallbackData: {
+    prizes: PageObject[]
+    workExperience: PageObject[]
+    workExperienceGenre: PageObject[]
+    events: PageObject[]
+  }
+}
+
+const About: NextPage<aboutDataType> = ({ fallbackData }) => {
+  console.log(fallbackData)
   if (!fallbackData) return <div>Loading...</div>
-  return <AboutContent data={fallbackData} />
+  return <AboutContent fallbackData={fallbackData} />
 }
 
 export default About
 
-export const getStaticProps: GetStaticProps = async ({ locale = 'ja' }) => {
-  const Data = aboutData
+export const getStaticProps: GetStaticProps<aboutDataType> = async ({ locale = 'ja' }) => {
+  // const page = await getPage(pageId)
+  const prizesDB = await getDatabase(prizesDBId)
+  const workExperienceDB = await getDatabase(workExperienceDBId)
+  const workExperienceGenreDB = await getDatabase(workExperienceGenreDBId)
+  const eventDB = await getDatabase(eventDBId)
+  const Data = {
+    // page: page,
+    prizes: prizesDB,
+    workExperience: workExperienceDB,
+    workExperienceGenre: workExperienceGenreDB,
+    events: eventDB,
+  }
+
   return {
     props: {
       fallbackData: Data,
