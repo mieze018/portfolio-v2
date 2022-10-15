fc701300ff1f28da13715881c38c9be33e8dc33e
Ayu Nakata <mieze018@users.noreply.github.com>
2022-10-16 02:27:25 +0900

    関連ファイルを削除

--- components/Molecules/TopBar.stories.tsx
+++
@@ -1,7 +0,0 @@
-import { TopBar } from './Header/TopBar'
-
-import type { ComponentMeta, ComponentStoryObj } from '@storybook/react'
-
-export default { component: TopBar } as ComponentMeta<typeof TopBar>
-
-export const Default: ComponentStoryObj<typeof TopBar> = {}
