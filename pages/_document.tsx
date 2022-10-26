3d9f61ed9e7c9bc980629e41d4f9bbbedcf1b3d8
Ayu Nakata <mieze018@users.noreply.github.com>
2022-10-26 20:15:08 +0900

    swapでプロパティ重複していた

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,7 +40,7 @@ export default class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
             <link
-              href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&display=swap&display=optional"
+              href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&display=optional"
               rel="stylesheet"
             />
           </React.Fragment>,
