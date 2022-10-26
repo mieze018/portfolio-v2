4c6b156937f44f66a005dcd228715fc13f62ea0a
Ayu Nakata <mieze018@users.noreply.github.com>
2022-10-26 19:54:08 +0900

    `@next/next/google-font-display`に引っ掛かるので修正
    https://github.com/mieze018/portfolio-v2/actions/runs/3328287761/jobs/5504161786#step:4:32

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,11 +37,10 @@ export default class MyDocument extends Document {
             <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#da532c" />
             <meta name="theme-color" content="#ffffff" />
-            {/* web font */}
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
             <link
-              href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&display=swap"
+              href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&display=swap&display=optional"
               rel="stylesheet"
             />
           </React.Fragment>,
