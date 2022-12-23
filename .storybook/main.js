aaa8e27d89a3dda178047349a20821f5c387274e
Ayu Nakata <mieze018@users.noreply.github.com>
2022-12-23 13:14:56 +0900

    yarn format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
 module.exports = {
-  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": {
-    name: "@storybook/nextjs",
-    options: {}
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
   docs: {
-    docsPage: "automatic"
-  }
-};
+    docsPage: 'automatic',
+  },
+}
