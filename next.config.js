3c7244ba1a6413f4410d1730009c6396a32ab201
Ayu Nakata <mieze018@users.noreply.github.com>
2022-12-25 01:02:08 +0900

    コンパイラのstyled-componentsの設定を追加
    https://nextjs.org/docs/advanced-features/compiler#styled-components

--- next.config.js
+++ next.config.js
@@ -28,4 +28,30 @@ module.exports = withTwin({
     config.resolve.fallback = { fs: false, module: false, path: false }
     return config
   },
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: boolean | {
+      // Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+      displayName?: boolean,
+      // Enabled by default.
+      ssr?: boolean,
+      // Enabled by default.
+      fileName?: boolean,
+      // Empty by default.
+      topLevelImportPaths?: string[],
+      // Defaults to ["index"].
+      meaninglessFileNames?: string[],
+      // Enabled by default.
+      cssProp?: boolean,
+      // Empty by default.
+      namespace?: string,
+      // Not supported yet.
+      minify?: boolean,
+      // Not supported yet.
+      transpileTemplateLiterals?: boolean,
+      // Not supported yet.
+      pure?: boolean,
+    },
+  },
 })
