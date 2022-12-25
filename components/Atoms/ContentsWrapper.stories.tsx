0f84b6682b3cc1e8015a35af36f196b4538ade74
Ayu Nakata <mieze018@users.noreply.github.com>
2022-12-26 05:29:46 +0900

    Create ContentsWrapper.stories.tsx

---
+++ components/Atoms/ContentsWrapper.stories.tsx
@@ -0,0 +1,9 @@
+import { ContentsWrapper } from './ContentsWrapper'
+
+export default { component: ContentsWrapper }
+export const ContentsWrapperDefault = {
+  args: {
+    children: <div>ContentsWrapper</div>,
+    $key: '/',
+  },
+}
