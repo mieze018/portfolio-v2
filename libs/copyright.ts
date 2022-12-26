5c4fa9ffc82a6f530774e17986c87d7d9dd881c4
Ayu Nakata <mieze018@users.noreply.github.com>
2022-12-26 14:29:44 +0900

    storiesディレクトリ以外でも走るか？

--- libs/copyright.ts
+++ libs/copyright.ts
@@ -3,5 +3,6 @@ import { author } from 'pages/api/basics'
 export const copyright = () => {
   const now = new Date()
   const this_year = now.getFullYear()
+
   return `© 2009-${this_year} ${author}`
 }
