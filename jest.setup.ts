b4c0c845a48efde63d73c932a024b9495889c0e2
Ayu Nakata <mieze018@users.noreply.github.com>
2023-01-29 12:28:26 +0900

    Update jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,49 @@
 import '@testing-library/jest-dom/extend-expect'
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+// Mock t function
+export const t = (key: string, params?: any) => {
+  if (key === 'key.with.params') {
+    return `key.with.params.${params.param}`
+  }
+
+  return key
+}
+// Mock react-i18next
+i18n
+  .use(initReactI18next).init({
+    lng: 'ja',
+    fallbackLng: 'ja',
+
+
+
+    debug: true,
+
+    resources: {
+      ja: {},
+      en: {}
+    }
+  });
+
+jest.mock("next-i18next", () => ({
+  ...jest.requireActual("next-i18next"),
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+
+      debug: true,
+      i18n: {
+        language: "ja",
+        locales: ['ja', 'en'],
+        defaultLocale: 'ja',
+        addResourceBundle: () => jest.fn(),
+        changeLanguage: () => Promise.resolve(),
+      },
+    };
+  },
+  withTranslation: () => (Component: any) => {
+    Component.defaultProps = { ...Component.defaultProps, t }
+    return Component
+  }
+}));
