0d3da5b52dfa0b1e3b44c6a0875a84adda351cfe
Ayu Nakata <mieze018@users.noreply.github.com>
2023-01-29 12:30:12 +0900

    Update Introduction.tsx

--- components/Molecules/About/Introduction.tsx
+++ components/Molecules/About/Introduction.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'next-i18next'
 
-import { SectionWrapper, P } from 'components/Atoms/About/Atoms'
+import { SectionWrapper, P } from '@/components/Atoms/About/Atoms'
 
 export const Introduction = () => {
   const { t } = useTranslation('common')
