38098a15c34391304eac73ee0e9629412dd0e6f5
Ayu Nakata <mieze018@users.noreply.github.com>
2023-01-29 14:45:56 +0900

    アクションを追加

--- playwright.config.ts
+++ playwright.config.ts
@@ -99,10 +99,10 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'next dev',
+    url: 'http://localhost:3000',
+  },
 }
 
 export default config
