7e75d1634a37adc170db71b28b071db8be65807e
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-19 00:13:36 +0900

    wip 💡 インタラクションテストを追加

--- components/Atoms/ContentsWrapper.stories.tsx
+++ components/Atoms/ContentsWrapper.stories.tsx
@@ -1,9 +1,26 @@
 import { ContentsWrapper } from './ContentsWrapper'
 
-export default { component: ContentsWrapper }
-export const ContentsWrapperDefault = {
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof ContentsWrapper> = {
+  component: ContentsWrapper,
+}
+export default meta
+type Story = StoryObj<typeof ContentsWrapper>
+
+export const ContentsWrapperDefault: Story = {
   args: {
     children: <div>ContentsWrapper</div>,
     $key: '/',
   },
+  // TODO レイアウトまでのstoriesを実装してその中でリンクをくりっっ句などをトリガーに実装した方が良さそう
+  // play: async ({ args, canvasElement, step, updateArgs }) => {
+  //   const canvas = within(canvasElement)
+  //   await step('ページ移管時にトランジションする', async () => {
+  //     await waitFor(async () => {
+  //       updateArgs({ $key: '/some-page' })
+  //       expect(canvas.getByText('ContentsWrapper')).toBeTruthy()
+  //     })
+  //   })
+  // },
 }
