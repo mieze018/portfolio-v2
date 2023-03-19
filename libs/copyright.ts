8e32965f2b3c1947f43b7eef5b47e5316e9c970c
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-20 00:41:55 +0900

    🧪  テスト追加
    ハードコーディングしていた箇所を修正

--- libs/copyright.ts
+++ libs/copyright.ts
@@ -3,5 +3,7 @@ import { author } from 'pages/api/basics'
 export const copyright = () => {
   const now = new Date()
   const this_year = now.getFullYear()
-  return `© 2009-${this_year} ${author}`
+  const start_year = 2009
+  const copyrightPrefix = '©'
+  return `${copyrightPrefix} ${start_year} - ${this_year} ${author}`
 }
