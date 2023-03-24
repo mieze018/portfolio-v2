744002181bda0ebf5fc7c38d932efa61b39d63c7
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-24 23:15:55 +0900

    ⛏   E2Eテストを実装

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('トップページにアクセスする', () => {
+  it('タイトルが表示される', () => {
+    cy.visit('/')
+    cy.contains('mieze illustration')
   })
 })
