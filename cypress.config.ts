e2024195cae65a6dac7911e27abd3ce33bda7840
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-25 00:26:19 +0900

    🧰  baseurlを設定

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: 'p3ih1o',
   e2e: {
+    baseUrl: 'http://localhost:3000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
