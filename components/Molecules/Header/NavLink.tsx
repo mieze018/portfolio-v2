317d80e88bbe3243fda7c15c8a91f3c73f499eca
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-24 23:43:52 +0900

    🛠  アップデートでLinkコンポーネントの中にaタグ入れるのは禁止になったので対応
    前まで確かLinkでonClickとかができなかった

--- components/Molecules/Header/NavLink.tsx
+++ components/Molecules/Header/NavLink.tsx
@@ -5,7 +5,7 @@ import tw, { styled } from 'twin.macro'
 
 import { contentsWrapperState } from 'libs/states/atoms'
 
-const A = styled.a<{ isCurrent: boolean }>`
+const Span = styled.span<{ isCurrent: boolean }>`
   ${tw`inline-block cursor-pointer mix-blend-multiply xs:tracking-widest [word-spacing:-0.2em]`}
   ${({ isCurrent }) => isCurrent && tw`underline hover:text-secondary`}
 `
@@ -22,18 +22,19 @@ export const NavLinks = ({
   return (
     <>
       {routes.map((route) => {
-        const currentPath = router.pathname === route.pathname
-        const toOnclickScroll = route.pathname === '/' || currentPath
+        const isLinkToCurrentPath = router.pathname === route.pathname
+        /** コンテンツまでスクロールさせるかどうか */
+        const isToScrollToContentWrapper = route.pathname === '/' || isLinkToCurrentPath
         return (
-          <Link href={route.pathname} key={route.pathname} scroll={toOnclickScroll ? false : true}>
-            <A
-              onClick={() =>
-                toOnclickScroll && setTimeout(() => contentsWrapper?.scrollIntoView(), 100)
-              }
-              isCurrent={currentPath}
-            >
-              {route.name}
-            </A>
+          <Link
+            href={route.pathname}
+            key={route.pathname}
+            scroll={!isToScrollToContentWrapper}
+            onClick={() =>
+              isToScrollToContentWrapper && setTimeout(() => contentsWrapper?.scrollIntoView(), 100)
+            }
+          >
+            <Span isCurrent={isLinkToCurrentPath}>{route.name}</Span>
           </Link>
         )
       })}
