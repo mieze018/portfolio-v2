8256ce2d096158c8ffa1ddd3668090290db1efc3
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-25 11:42:13 +0900

    🧰  huskyを設定

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   projectId: 'p3ih1o',
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3009',
     defaultCommandTimeout: 10000,
+    //実行前にサーバを起動する
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
