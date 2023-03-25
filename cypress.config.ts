a4f21dbc109cce756f032a8826a6a45e98758db9
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-25 10:46:47 +0900

    🧰  タイムアウトが早すぎて失敗するので伸ばした

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   projectId: 'p3ih1o',
   e2e: {
     baseUrl: 'http://localhost:3000/',
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
