78d477980f52d2c8feb3a8e01e46a53d9355f9bb
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-25 10:35:15 +0900

    ⛏   国際化テストを追加

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,21 @@
-describe('トップページにアクセスする', () => {
+describe('トップページにアクセスしたとき', () => {
   it('タイトルが表示される', () => {
     cy.visit('/')
     cy.contains('mieze illustration')
   })
+  describe('ブラウザの言語が英語の時', () => {
+    it('/en/[path] にリダイレクトされる', () => {
+      cy.visit('/', { headers: { 'Accept-Language': 'en' } })
+      cy.url().should('match', /\/en$/)
+    })
+    it('/about を表示すると "Ayu Nakata"と表示される', () => {
+      cy.visit('/en/about', { headers: { 'Accept-Language': 'en' } })
+      //urlが'/en/about'になるのを待つ
+      cy.url().should('eq', Cypress.config().baseUrl + '/en/about')
+      //data-testid="about-name"の要素を取得
+      cy.get('[data-testid="author"]').contains('Ayu Nakata')
+    })
+  })
+})
+
 })
