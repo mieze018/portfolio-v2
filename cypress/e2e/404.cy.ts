1a7c0535b508eafb4c6a8dfe133ab1e970215e55
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-25 10:42:09 +0900

    💡 試行回数をケチるために1ファイルにまとめた

--- cypress/e2e/404.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('ルーティングが存在しないページにアクセスする', () => {
-  it('トップページにリダイレクトされる', () => {
-    cy.visit('/undefined_page_url', { failOnStatusCode: false })
-    //urlが`undefined_page_url`を含まなくなるのを待つ
-    cy.url().should('not.include', 'undefined_page_url')
-    // urlがbaseUrlになることを確認する
-    cy.url().should('eq', Cypress.config().baseUrl)
-  })
-})
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -18,4 +18,13 @@ describe('トップページにアクセスしたとき', () => {
   })
 })
 
+//404
+describe('ルーティングが存在しないページにアクセスしたとき', () => {
+  it('トップページにリダイレクトされる', () => {
+    cy.visit('/undefined_page_url', { failOnStatusCode: false })
+    //urlが`undefined_page_url`を含まなくなるのを待つ
+    cy.url().should('not.include', 'undefined_page_url')
+    // urlがbaseUrlになることを確認する
+    cy.url().should('eq', Cypress.config().baseUrl)
+  })
 })
