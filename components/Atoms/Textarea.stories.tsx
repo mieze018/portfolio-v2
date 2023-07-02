e1c4ad65bb02bb4c36980027f42cc2036a6a6c7a
Ayu Nakata <mieze018@users.noreply.github.com>
2023-07-02 21:41:28 +0900

    textareaが自動でリサイズすることを確認するVRTを作成

--- components/Atoms/Textarea.stories.tsx
+++ components/Atoms/Textarea.stories.tsx
@@ -2,3 +2,17 @@ import { Textarea } from './Textarea'
 
 export default { component: Textarea }
 export const TextareaDefault = { args: { children: 'Textarea' } }
+export const AutoResize = {
+  args: {
+    children: `
+いろはにほへとちりぬるをわかよたれそつねならむうゐのおくやまけふこえてあさきゆめみしゑひもせす
+
+いろはにほへとちりぬるをわかよたれそつねならむうゐのおくやまけふこえてあさきゆめみしゑひもせす
+
+いろはにほへとちりぬるをわかよたれそつねならむうゐのおくやまけふこえてあさきゆめみしゑひもせす
+
+いろはにほへとちりぬるをわかよたれそつねならむうゐのおくやまけふこえてあさきゆめみしゑひもせす
+
+      `,
+  },
+}
