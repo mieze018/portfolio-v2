dc2c2985993395881b47267dc172c992e8486f74
Ayu Nakata <mieze018@users.noreply.github.com>
2023-11-18 17:44:59 +0900

    不要なライブラリとその読み込みを削除

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,6 @@ module.exports = {
     '@storybook/addon-interactions',
     '@storybook/addon-actions',
     '@storybook/addon-a11y',
-    '@storybook/addon-mdx-gfm',
   ],
   framework: {
     name: '@storybook/nextjs',
