6531c68ca99e5ab2330acdfa69c2a350e8fb6bcb
Ayu Nakata <mieze018@users.noreply.github.com>
2023-11-18 17:50:32 +0900

    重複しているaddon読み込みを削除

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,6 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     // 'storybook-addon-swc',
-    '@storybook/addon-interactions',
     '@storybook/addon-actions',
     '@storybook/addon-a11y',
   ],
