14a90ddb6c5972a939b9229eacf5de58f7d4fe6c
Ayu Nakata <mieze018@users.noreply.github.com>
2023-11-19 14:48:05 +0900

    画像拡大モーダルのStoryを仮作成(要改修)

---
+++ components/Organisms/Modal.stories.tsx
@@ -0,0 +1,29 @@
+import { expect } from '@storybook/jest'
+import { userEvent, waitFor, within } from '@storybook/testing-library'
+import type { Meta, StoryObj } from '@storybook/react'
+import { Modal } from './Modal'
+import { useSetAtom } from 'jotai'
+import { modalContentState } from 'libs/states/atoms'
+
+const meta: Meta<typeof Modal> = {
+  component: Modal,
+  parameters: {
+    url: 'http://example.com#closeup',
+    // TODO: ロジック切り離したら消す
+    chromatic: { disableSnapshot: true },
+  },
+}
+export default meta
+type Story = StoryObj<typeof Modal>
+
+export const Default: Story = () => {
+  // 内容を設定
+  const setModalContent = useSetAtom(modalContentState)
+  setModalContent(<div>モーダルコンテンツ</div>)
+
+  return <Modal />
+}
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const body = within(canvasElement.parentNode as HTMLElement) // 内容がPortalなのでルートの親から探す
+}
