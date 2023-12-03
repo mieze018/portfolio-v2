be16e2817adb73a97453f13e4362bba8986a3d42
Ayu Nakata <mieze018@users.noreply.github.com>
2023-12-03 17:18:12 +0900

    Photo

---
+++ components/Molecules/Post/Photo.stories.tsx
@@ -0,0 +1,35 @@
+import { expect } from '@storybook/jest'
+import { userEvent, waitFor, within } from '@storybook/testing-library'
+import type { Meta, StoryObj } from '@storybook/react'
+import { Photo } from './Photo'
+
+const meta: Meta<typeof Photo> = { component: Photo }
+export default meta
+type Story = StoryObj<typeof Photo>
+
+export const Default: Story = {
+  args: {
+    photo: {
+      caption: 'caption',
+      original_size: {
+        url: 'https://placehold.jp/800x600.png',
+        height: 600,
+        width: 800,
+      },
+      alt_sizes: [
+        {
+          url: 'https://placehold.jp/100x100.png',
+          height: 100,
+          width: 100,
+        },
+      ],
+    },
+  },
+}
+export const Column: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    isColumn: true,
+  },
+}
