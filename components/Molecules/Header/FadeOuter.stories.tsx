e40404df5415c7fd6e43a8252d9fce8125025b74
Ayu Nakata <mieze018@users.noreply.github.com>
2023-12-03 15:10:06 +0900

    FadeOuter

---
+++ components/Molecules/Header/FadeOuter.stories.tsx
@@ -0,0 +1,35 @@
+import { expect } from '@storybook/jest'
+import { userEvent, waitFor, within } from '@storybook/testing-library'
+import type { Meta, StoryObj } from '@storybook/react'
+import { FadeOuter } from './FadeOuter'
+
+const meta: Meta<typeof FadeOuter> = {
+  component: FadeOuter,
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ height: '100vh' }} data-testid="scroll-wrapper">
+          <div style={{ top: 0, position: 'absolute' }}>
+            <Story />
+          </div>
+        </div>
+      )
+    },
+  ],
+}
+export default meta
+type Story = StoryObj<typeof FadeOuter>
+
+export const Default: Story = { args: { children: 'FadeOuter' } }
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const fadeOuter = canvas.getByText('FadeOuter')
+  // スクロールダウンすると、FadeOuterが消える
+  window.scrollTo(0, 500)
+  await waitFor(() => expect(fadeOuter).not.toBeVisible())
+  // scrollFaderの位置は、top: 0px
+  await expect(fadeOuter.scrollTop === 0).toBeTruthy()
+  //上部へスクロールすると、FadeOuterが表示される
+  window.scrollTo(0, 0)
+  await waitFor(() => expect(fadeOuter).toBeVisible())
+}
