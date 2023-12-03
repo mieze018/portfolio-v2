d8349d0a8d861f682a53b704994834267db1cfda
Ayu Nakata <mieze018@users.noreply.github.com>
2023-12-03 17:18:02 +0900

    WIP WaterSurface

---
+++ components/Molecules/Header/WaterSurface.stories.tsx
@@ -0,0 +1,19 @@
+import { expect } from '@storybook/jest'
+import { userEvent, waitFor, within } from '@storybook/testing-library'
+import type { Meta, StoryObj } from '@storybook/react'
+import { WaterSurface } from './WaterSurface'
+
+const meta: Meta<typeof WaterSurface> = { component: WaterSurface }
+export default meta
+type Story = StoryObj<typeof WaterSurface>
+
+export const Default: Story = { args: { children: 'WaterSurface' } }
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const body = within(canvasElement.parentNode as HTMLElement) // 内容がPortalなのでルートの親から探す
+}
+
+export const Hover = { ...Default, parameters: { pseudo: { hover: true } } }
+export const Focus = { ...Default, parameters: { pseudo: { focus: true } } }
+export const Active = { ...Default, parameters: { pseudo: { active: true } } }
+export const FocusVisible = { ...Default, parameters: { pseudo: { focusVisible: true } } }
