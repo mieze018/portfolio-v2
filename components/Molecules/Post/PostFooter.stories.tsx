c7fa624f8a5d9b44db86337eeae82a7d3761b55c
Ayu Nakata <mieze018@users.noreply.github.com>
2023-12-03 17:18:30 +0900

    PostFooter

---
+++ components/Molecules/Post/PostFooter.stories.tsx
@@ -0,0 +1,14 @@
+import { expect } from '@storybook/jest'
+import { userEvent, waitFor, within } from '@storybook/testing-library'
+import type { Meta, StoryObj } from '@storybook/react'
+import { PostFooter } from './PostFooter'
+
+const meta: Meta<typeof PostFooter> = { component: PostFooter }
+export default meta
+type Story = StoryObj<typeof PostFooter>
+
+export const Default: Story = {
+  args: {
+    postDate: '2021-01-01 00:00:00 GMT',
+  },
+}
