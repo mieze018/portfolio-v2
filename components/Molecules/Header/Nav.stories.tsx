140bffffef82250ca8e94f011516adc03bf1b82d
Ayu Nakata <mieze018@users.noreply.github.com>
2023-12-03 17:04:10 +0900

    Nav インタラクションテストはスタイルはついてるはずだけど最終的にfilter:noneになるので保留

---
+++ components/Molecules/Header/Nav.stories.tsx
@@ -0,0 +1,37 @@
+import { expect } from '@storybook/jest'
+import { userEvent, waitFor, within } from '@storybook/testing-library'
+import type { Meta, StoryObj } from '@storybook/react'
+import { Nav } from './Nav'
+
+const meta: Meta<typeof Nav> = {
+  component: Nav,
+  decorators: [
+    (Story) => {
+      return (
+        <div style={{ height: '100vh' }} data-testid="scroll-wrapper">
+          <div style={{ top: 0, position: 'fixed' }}>
+            <Story />
+          </div>
+        </div>
+      )
+    },
+  ],
+}
+export default meta
+type Story = StoryObj<typeof Nav>
+
+export const Default: Story = { args: { children: 'Nav' } }
+// Default.play = async ({ canvasElement }) => {
+//   const canvas = within(canvasElement)
+//   const Nav = canvas.getByText('Nav')
+//   // スクロールダウンすると、Navに`filter: brightness(125%)`及び、@media (min-width: 768px)なら `blur(1px)`,それ以外なら`blur(0px)`が適用される
+//   window.scrollTo(0, 500)
+//   // コンポーネント内のuseScrollState()の返り値が、{ init: false, sinking: true, sunk: false }になるまで待つ
+
+//   await waitFor(() => expect(Nav).toHaveStyle({ filter: 'blur(1px) brightness(1.25)' }))
+//   // scrollFaderの位置は、top: 0px
+//   await expect(Nav.scrollTop === 0).toBeTruthy()
+//   //上部へスクロールすると、Navに`filter: brightness(100%) blur(0)`が適用される
+//   window.scrollTo(0, 0)
+//   await waitFor(() => expect(Nav).toHaveStyle({ filter: 'blur(0px) brightness(1)' }))
+// }
