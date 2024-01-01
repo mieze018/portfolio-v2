0ca0c0a73170ae0b55163adfee2a4191171913c3
mieze018 <mieze018@users.noreply.github.com>
2024-01-01 14:03:21 +0900

    Nav

--- components/Molecules/Header/Nav.stories.tsx
+++ components/Molecules/Header/Nav.stories.tsx
@@ -8,7 +8,7 @@ const meta: Meta<typeof Nav> = {
   decorators: [
     (Story) => {
       return (
-        <div style={{ height: '100vh' }} data-testid="scroll-wrapper">
+        <div style={{ height: '200vh' }} data-testid="scroll-wrapper">
           <div style={{ top: 0, position: 'fixed' }}>
             <Story />
           </div>
@@ -21,17 +21,21 @@ export default meta
 type Story = StoryObj<typeof Nav>
 
 export const Default: Story = { args: { children: 'Nav' } }
-// Default.play = async ({ canvasElement }) => {
-//   const canvas = within(canvasElement)
-//   const Nav = canvas.getByText('Nav')
-//   // スクロールダウンすると、Navに`filter: brightness(125%)`及び、@media (min-width: 768px)なら `blur(1px)`,それ以外なら`blur(0px)`が適用される
-//   window.scrollTo(0, 500)
-//   // コンポーネント内のuseScrollState()の返り値が、{ init: false, sinking: true, sunk: false }になるまで待つ
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const user = userEvent.setup()
+  const Nav = canvas.getByText('Nav')
+  // スクロールダウンすると、Navに`filter: brightness(125%)`及び、@media (min-width: 768px)なら `blur(1px)`,それ以外なら`blur(0px)`が適用される
+  window.scrollTo(0, 500)
+  // user.scroll(canvas.getByTestId('scroll-wrapper'), 0, 500)
+  // コンポーネント内のuseScrollState()の返り値が、{ init: false, sinking: true, sunk: false }になるまで待つ
 
-//   await waitFor(() => expect(Nav).toHaveStyle({ filter: 'blur(1px) brightness(1.25)' }))
-//   // scrollFaderの位置は、top: 0px
-//   await expect(Nav.scrollTop === 0).toBeTruthy()
-//   //上部へスクロールすると、Navに`filter: brightness(100%) blur(0)`が適用される
-//   window.scrollTo(0, 0)
-//   await waitFor(() => expect(Nav).toHaveStyle({ filter: 'blur(0px) brightness(1)' }))
-// }
+  await waitFor(() => expect(Nav).toHaveStyle({ filter: 'blur(1px) brightness(1.25)' }))
+  // scrollFaderの位置は、top: 0px
+  await expect(Nav.scrollTop === 0).toBeTruthy()
+  //上部へスクロールすると、Navに`filter: brightness(100%) blur(0)`が適用される
+  window.scrollTo(0, 0)
+  await waitFor(() => expect(Nav).toHaveStyle({ filter: 'blur(0px) brightness(1)' }))
+}
+
+export const Footer: Story = { args: { children: 'Nav', footer: true } }
