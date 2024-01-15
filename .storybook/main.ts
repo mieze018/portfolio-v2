4eabc870abab69b54b9cb21357300afb07ae35a5
mieze018 <mieze018@users.noreply.github.com>
2024-01-15 10:18:36 +0900

    refactor: preview.jsをtsに変更

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,22 @@
+import { dirname, join } from 'path'
 import type { StorybookConfig } from '@storybook/nextjs'
 const config: StorybookConfig = {
   stories: ['../components/**/*.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-actions',
-    '@storybook/addon-a11y',
-    'storybook-react-i18next',
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-actions'),
+    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('storybook-react-i18next'),
   ],
   framework: {
-    name: '@storybook/nextjs',
-    options: {},
+    name: getAbsolutePath('@storybook/nextjs'),
+    options: {
+      builder: {
+        useSWC: true, // Enables SWC support
+      },
+    },
   },
   docs: {
     autodocs: true,
@@ -21,3 +26,7 @@ const config: StorybookConfig = {
   },
 }
 export default config
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
