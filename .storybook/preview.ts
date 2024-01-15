7fbe33a6f9481bdea9e05cba0f73fb6b689fd9b4
mieze018 <mieze018@users.noreply.github.com>
2024-01-15 10:18:36 +0900

    /

---
+++ .storybook/preview.ts
@@ -0,0 +1,24 @@
+import type { Preview } from '@storybook/nextjs'
+import { decoratorsComponent } from './decorators'
+import i18n from './i18next.js'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  i18n,
+  locale: 'ja',
+  locales: {
+    en: 'English',
+    ja: '日本語',
+  },
+}
+
+export default preview
+export const decorators = [decoratorsComponent]
