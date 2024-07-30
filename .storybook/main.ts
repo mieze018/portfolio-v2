69a14e06e0e15c709a3218f32b5e7d01ffb7be8f
Ayu Nakata <ayu.nakata@mugenup.com>
2024-07-31 03:29:04 +0900

    Update main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,31 +1,24 @@
-import { dirname, join } from 'path'
 import type { StorybookConfig } from '@storybook/nextjs'
 const config: StorybookConfig = {
-  stories: ['../components/**/*.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@storybook/addon-actions'),
-    getAbsolutePath('@storybook/addon-a11y'),
-    getAbsolutePath('storybook-react-i18next'),
-    getAbsolutePath('@chromatic-com/storybook'),
-  ],
   framework: {
-    name: getAbsolutePath('@storybook/nextjs'),
+    name: '@storybook/nextjs',
     options: {
       builder: {
-        useSWC: true, // Enables SWC support
+        useSWC: true,
       },
     },
   },
+  stories: ['../components/**/*.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-actions',
+    '@storybook/addon-a11y',
+    'storybook-react-i18next',
+    '@chromatic-com/storybook',
+  ],
   docs: {},
-  typescript: {
-    check: false,
-  },
+  typescript: { check: false },
 }
 export default config
-
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
-}
