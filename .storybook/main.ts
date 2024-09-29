19b7862cea28631c19cd98207570200e751d8f7d
Ayu Nakata <mieze018@users.noreply.github.com>
2024-09-29 10:12:22 +0900

    upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,6 +10,7 @@ const config: StorybookConfig = {
     '@storybook/addon-a11y',
     // 'storybook-react-i18next',
     '@storybook/addon-webpack5-compiler-babel',
+    '@chromatic-com/storybook',
   ],
   framework: {
     name: '@storybook/nextjs',
