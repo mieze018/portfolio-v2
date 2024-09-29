3081553449ccd6c777923529faa75839a28ba50e
Ayu Nakata <mieze018@users.noreply.github.com>
2024-09-29 09:17:10 +0900

    @storybook/addon-webpack5-compiler-babel を追加、preview.jsをts化

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,20 @@
 import type { StorybookConfig } from '@storybook/nextjs'
+
 const config: StorybookConfig = {
   stories: ['../components/**/*.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    // 'storybook-addon-swc',
     '@storybook/addon-actions',
     '@storybook/addon-a11y',
     'storybook-react-i18next',
+    '@storybook/addon-webpack5-compiler-babel',
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
-  docs: {
-    autodocs: true,
-  },
-  typescript: {
-    check: false,
-    // TODO: ローカルでだけ立ち上がらないので一旦無効化している。SBのアップデート後に再度試す
-    reactDocgen: false,
-  },
+  staticDirs: ['..\\public'],
 }
 export default config
---
+++ .storybook/preview.ts
@@ -0,0 +1,23 @@
+import type { Preview } from '@storybook/react'
+// import { decoratorsComponent } from './decorators'
+// import i18n from './i18next.js'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    // i18n,
+    // locale: 'ja',
+    // locales: {
+    //   en: 'English',
+    //   ja: '日本語',
+    // },
+  },
+}
+
+export default preview
+// export const decorators = [decoratorsComponent]
