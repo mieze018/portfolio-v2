40a3919e9e3c9eaf2555f552cb26c5dd07603ae5
Ayu Nakata <mieze018@users.noreply.github.com>
2024-09-30 01:31:13 +0900

    不要なstoriesを削除

--- components/Molecules/Header/Floater.stories.tsx
+++ components/Molecules/Header/Floater.stories.tsx
@@ -11,8 +11,3 @@ Default.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement)
   const body = within(canvasElement.parentNode as HTMLElement) // 内容がPortalなのでルートの親から探す
 }
-
-export const Hover = { ...Default, parameters: { pseudo: { hover: true } } }
-export const Focus = { ...Default, parameters: { pseudo: { focus: true } } }
-export const Active = { ...Default, parameters: { pseudo: { active: true } } }
-export const FocusVisible = { ...Default, parameters: { pseudo: { focusVisible: true } } }
