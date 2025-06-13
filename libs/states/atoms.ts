83a74644bca4fa1331f4043d4b45360cda254286
Ayu Nakata <mieze018@users.noreply.github.com>
2025-06-13 23:42:12 +0900

    アップグレードコマンドを実行 npx @next/codemod@canary upgrade latest

--- libs/states/atoms.ts
+++ libs/states/atoms.ts
@@ -1,5 +1,7 @@
 import { atom } from 'jotai'
 
+import type { JSX } from "react";
+
 export const contentsWrapperState = atom<HTMLElement | null>(null)
 
 export const modalContentState = atom<JSX.Element | null>(null)
