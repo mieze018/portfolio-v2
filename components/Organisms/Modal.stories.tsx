fe2deaba168b516886c46006404d6daf0cb98566
Ayu Nakata <mieze018@users.noreply.github.com>
2025-06-15 13:57:02 +0900

    refactor(frontend): モーダルのストーリー設定を更新し、テスト用のハッシュを追加

--- components/Organisms/Modal.stories.tsx
+++ components/Organisms/Modal.stories.tsx
@@ -1,28 +1,61 @@
-import { within } from 'storybook/test'
 import type { Meta, StoryObj } from '@storybook/nextjs'
-import { Modal } from './Modal'
+import { hashCloseup, Modal } from './Modal'
 import { useSetAtom } from 'jotai'
 import { modalContentState } from 'libs/states/atoms'
+
+const testImageThumb = 'https://picsum.photos/id/4/400/300'
+const testImageFull = 'https://picsum.photos/id/4/1200/800'
 
 const meta: Meta<typeof Modal> = {
   component: Modal,
   parameters: {
-    url: 'http://example.com#closeup',
-    // TODO: ロジック切り離したら消す
     chromatic: { disableSnapshot: true },
   },
+  decorators: [
+    (Story) => {
+      const setModalContent = useSetAtom(modalContentState)
+      setModalContent(<img src={testImageFull} alt="サンプル画像" />)
+      return (
+        <div style={{ padding: '20px' }}>
+          <a href={`#${hashCloseup}`} data-testid="modal-trigger">
+            <img src={testImageThumb} alt="サンプル画像" />
+          </a>
+          <Story />
+        </div>
+      )
+    },
+  ],
 }
 export default meta
 type Story = StoryObj<typeof Modal>
 
-export const Default: Story = () => {
-  // 内容を設定
-  const setModalContent = useSetAtom(modalContentState)
-  setModalContent(<div>モーダルコンテンツ</div>)
+export const Default: Story = {}
 
-  return <Modal />
-}
-Default.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement)
-  const body = within(canvasElement.parentNode as HTMLElement) // 内容がPortalなのでルートの親から探す
+// todo: URLへのハッシュ付与がテストできないので要リファクタ
+// export const OpenModal: Story = {
+//   ...Default,
+//   play: async ({ canvasElement }) => {
+//     const canvas = within(canvasElement)
+
+//     // モーダルトリガーをクリック
+//     const trigger = canvas.getByTestId('modal-trigger')
+//     await userEvent.click(trigger)
+
+//     // モーダルが開いていることを確認
+//     await waitFor(() => {
+//       const modal = canvas.getByRole('dialog')
+//       expect(modal).toBeInTheDocument()
+//     })
+//   },
+// }
+
+export const InitiallyOpen: Story = {
+  ...Default,
+  parameters: {
+    nextjs: {
+      router: {
+        asPath: `/#${hashCloseup}`,
+      },
+    },
+  },
 }
