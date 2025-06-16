32926cc46bf57bbfd4fb3c2d2ae019339a501315
Ayu Nakata <mieze018@users.noreply.github.com>
2025-06-16 23:35:03 +0900

    fix: add missing subsets for Noto Serif JP and EB Garamond fonts

--- libs/fonts.ts
+++ libs/fonts.ts
@@ -1,8 +1,10 @@
 import { EB_Garamond, Noto_Serif_JP } from 'next/font/google'
 
 export const notoSerifJP = Noto_Serif_JP({
+  subsets: ['latin'],
   variable: '--font-jp',
 })
 export const ebGaramond = EB_Garamond({
+  subsets: ['latin'],
   variable: '--font-en',
 })
