2393cc94af609785660a732d9a62a1fff5107e65
mieze018 <6752643+mieze018@users.noreply.github.com>
2026-04-06 03:09:40 +0900

    test: Aboutページの結合VRTを強化

--- components/Organisms/AboutContent.stories.tsx
+++ components/Organisms/AboutContent.stories.tsx
@@ -1,4 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite'
+import { Default as mockEvents } from 'components/Molecules/About/Events.stories'
+import { Default as PrizesDefault } from 'components/Molecules/About/Prizes.stories'
+import { Default as WorkDefault } from 'components/Molecules/About/WorkExperience.stories'
+import type { PageObject } from 'libs/@type/api/notion'
 import { mockSocialLinks } from 'libs/test/mockSocialLinks'
 import { AboutContent } from './AboutContent'
 
@@ -10,10 +14,10 @@ export const Default: Story = {
   args: {
     socialLinks: mockSocialLinks,
     fallbackData: {
-      prizes: [],
-      workExperience: [],
-      workExperienceGenre: [],
-      events: [],
+      prizes: PrizesDefault.args?.prizes as PageObject[],
+      workExperience: WorkDefault.args?.workExperience as PageObject[],
+      workExperienceGenre: WorkDefault.args?.genres as PageObject[],
+      events: mockEvents.args?.events as PageObject[],
     },
   },
 }
