0595be85c9455eea921f96976d83941795e115cc
mieze018 <6752643+mieze018@users.noreply.github.com>
2026-04-05 22:28:11 +0900

    feat: ページ下部のSNSアイコンを調整

--- components/Molecules/SocialLinks.tsx
+++ components/Molecules/SocialLinks.tsx
@@ -16,7 +16,7 @@ import {
 const Wrapper = tw(
   'div',
   cva(
-    'flex items-center justify-center m-auto w-full gap-4 md:gap-4 text-4xl md:text-2xl flex-wrap'
+    'flex items-center justify-center m-auto w-full gap-4 md:gap-4 text-3xl md:text-2xl flex-wrap px-1'
   )
 )
 
